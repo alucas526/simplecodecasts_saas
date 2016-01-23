2cccd6f21389aeda1208ccf67a47092321c2a231
Alan Lucas <alucas@gmail.com>
2016-01-23 03:04:46 +0000

    Improved the About page link (and added a Hello page for fun).

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   get '/about' => 'pages#about'
+  get '/hello' => 'pages#hello'
   root 'pages#home'
 
   # Example of regular route:
