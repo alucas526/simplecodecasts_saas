571054b4a7ea2538a2f654b099c1a48d5207e3c3
Alan Lucas <alucas@gmail.com>
2016-02-13 22:27:55 +0000

    Updates to save new contacts. Added flash statuses to Contacts page.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,5 +6,19 @@ class ContactsController < ApplicationController
     end
     
     def create
+        @contact = Contact.new(contact_params)
+        
+        if @contact.save 
+            flash[:success] = 'Message sent.'
+            redirect_to new_contact_path
+        else 
+            flash[:danger] = 'Error Occurred. Message not sent.'
+            redirect_to new_contact_path
+        end
     end
+    
+    private
+        def contact_params
+            params.required(:contact).permit(:name, :email, :comments)
+        end
 end
