7181c1767e81a0760cdf4691d18a2e9580f6b73d
Alan Lucas <alucas@gmail.com>
2016-04-23 17:10:12 +0000

    Added file fields for image upload on profiles, updated show view to display avatar.

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,4 +4,9 @@ class Profile < ActiveRecord::Base
     validates :last_name, presence: true
     validates :job_title, presence: true
     validates :contact_email, presence: true
+    
+    # Paperclip code for profile image attachment
+    has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+    validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+    # End Paperclip code for profile image attachment
 end
